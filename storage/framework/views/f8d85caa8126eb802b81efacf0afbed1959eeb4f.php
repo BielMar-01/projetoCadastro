54a5b34e2c434b9fe74c1478da07d9221590ebcc
Gabriel Marques <bielmarques922@gmail.com>
2020-01-22 18:16:42 -0300

    Preparando Index e layout

---
+++ storage/framework/views/f8d85caa8126eb802b81efacf0afbed1959eeb4f.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cadastro de Produtos</title>
+
+    <link rel="stylesheet" href="<?php echo e(asset('css/app.css')); ?>">
+</head>
+<body>
+    <div class="container">
+        <main role="main">
+            <?php if (! empty(trim($__env->yieldContent('body')))): ?>
+                <?php echo $__env->yieldContent('body'); ?>
+            <?php endif; ?>
+        </main>
+    </div>
+
+    <script src="<?php echo e(asset('js/app.js')); ?>" type="text/javascript"></script>
+</body>
+</html><?php /**PATH /home/gabriel/code/projetoCadastro/resources/views/layout/app.blade.php ENDPATH**/ ?>
