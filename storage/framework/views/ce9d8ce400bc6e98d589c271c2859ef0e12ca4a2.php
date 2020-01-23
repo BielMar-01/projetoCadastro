ad70d3c202e1f0fd2055f18f05e4f95aa71919bb
Gabriel Marques <bielmarques922@gmail.com>
2020-01-23 17:16:24 -0300

    Formulário - Novo Departamento - Salvando registros

---
+++ storage/framework/views/ce9d8ce400bc6e98d589c271c2859ef0e12ca4a2.php
@@ -0,0 +1,17 @@
+<?php $__env->startSection('body'); ?>
+    <div class="card border">
+        <div class="card-body">
+            <form action="/categorias" method="POST">
+                <?php echo csrf_field(); ?>
+                <div class="form-group">
+                    <label for="nomeCategoria">Nome da Categoria</label>
+                    <input type="text" class="form-control" name="nomeCategoria" id="nomeCategoria" 
+                            placeholder="Categoria">
+                </div>
+                <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
+                <button type="cancel" class="btn btn-danger btn-sm">Cancelar</button>
+            </form>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layout.app', ["current" => "categorias"], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/gabriel/code/projetoCadastro/resources/views/novacategoria.blade.php ENDPATH**/ ?>
