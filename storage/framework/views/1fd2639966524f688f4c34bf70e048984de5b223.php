2f9557c54c9eea996d36c000f86738947c9f78b4
Gabriel Marques <bielmarques922@gmail.com>
2020-01-23 22:26:20 -0300

    Listando Categorias

--- storage/framework/views/1fd2639966524f688f4c34bf70e048984de5b223.php
+++ storage/framework/views/1fd2639966524f688f4c34bf70e048984de5b223.php
@@ -1,4 +1,34 @@
 <?php $__env->startSection('body'); ?>
-    <h4>Página de Categorias</h4>
+    <div class="card border">
+        <div class="card-body">
+            <h5 class="card-title">Cadastro de Categorias</h5>
+            <?php if(count($cats) > 0): ?>
+            <table class="table table_ordered table-hover">
+                <thead>
+                    <tr>
+                        <th>Código</th>
+                        <th>Nome da Categoria</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $__currentLoopData = $cats; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $cat): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <tr>
+                            <td><?php echo e($cat->id); ?></td>
+                            <td><?php echo e($cat->nome); ?></td>
+                            <td>
+                                <a href="/categorias/editar/<?php echo e($cat->id); ?>" class="btn btn-sm btn-primary">Editar</a>
+                                <a href="/categorias/apagar/<?php echo e($cat->id); ?>" class="btn btn-sm btn-danger">Apagar</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                </tbody>
+            </table>
+            <?php endif; ?>
+        </div>
+        <div class="card-footer">
+            <a href="/categorias/novo" class="btn btn-sm btn-primary" role="button">Nova Categoria</a>
+        </div>
+    </div>
 <?php $__env->stopSection(); ?>
 <?php echo $__env->make('layout.app', ["current" => "categorias"], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/gabriel/code/projetoCadastro/resources/views/categorias.blade.php ENDPATH**/ ?>
